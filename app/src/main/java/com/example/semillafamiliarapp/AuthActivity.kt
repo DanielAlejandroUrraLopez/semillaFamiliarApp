b0b73cf2f36f0111eeed0fd7d6b9fd831e62340e
Daniel <daniel.desarrolloweb@outlook.com>
2020-12-09 20:33:18 -0300

    validación con google

--- app/src/main/java/com/example/semillafamiliarapp/AuthActivity.kt
+++ app/src/main/java/com/example/semillafamiliarapp/AuthActivity.kt
@@ -1,18 +1,28 @@
 package com.example.semillafamiliarapp
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
 import kotlinx.android.synthetic.main.activity_home.emailTextView
 import kotlinx.android.synthetic.main.activity_home.logOutButton
 
+private val GOOGLE_SIGN_IN = 100
+
 class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
@@ -24,6 +34,23 @@ class AuthActivity : AppCompatActivity() {
 
         //setup
         setup()
+        session()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if(email != null && provider != null){
+            authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
     }
 
     private fun setup(){
@@ -56,6 +83,18 @@ class AuthActivity : AppCompatActivity() {
             }
         }
 
+        googleButton.setOnClickListener{
+            // configuracion
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+
     }
 
     private fun showAlert(){
@@ -74,5 +113,36 @@ class AuthActivity : AppCompatActivity() {
             putExtra("provider", provider.name)
         }
         startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try{
+
+                val account = task.getResult(ApiException::class.java)
+
+                if(account != null){
+
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(account.email ?: "", ProviderType.GOOGLE)
+                        }else{
+                            showAlert()
+                        }
+                    }
+
+                }
+
+            }catch (e: ApiException){
+
+                showAlert()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/semillafamiliarapp/HomeActivity.kt
+++ app/src/main/java/com/example/semillafamiliarapp/HomeActivity.kt
@@ -1,12 +1,14 @@
 package com.example.semillafamiliarapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 enum class ProviderType {
-    BASIC
+    BASIC,
+    GOOGLE
 }
 
 class HomeActivity : AppCompatActivity() {
@@ -20,6 +22,12 @@ class HomeActivity : AppCompatActivity() {
         val email: String? = bundle?.getString("email")
         val provider: String? = bundle?.getString("provider")
         setup(email, provider)
+
+        // Guardado de datos
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
     }
 
     private fun setup(email: String?, provider: String?) {
@@ -29,6 +37,10 @@ class HomeActivity : AppCompatActivity() {
         providerTextView.text = provider
 
         logOutButton.setOnClickListener {
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
             FirebaseAuth.getInstance().signOut()
             onBackPressed()
         }
